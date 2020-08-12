2b9f45e8534b183a5351466a4dce1a2ddb4943b1
Kacper Białecki <kapits3@yahoo.com>
2020-08-12 03:54:16 +0200

    initial upload

---
+++ main.py
@@ -0,0 +1,81 @@
+import os
+import urllib.request, json, urllib.parse, requests
+import song
+
+
+def rann(map_path):
+    # Pick first .osu file
+    file = [x for x in os.listdir(map_path) if x.endswith(".osu")][0]
+    osu_path = os.path.join(map_path, file)
+    # print(osu_path)
+
+    f = open(osu_path, "r", encoding="UTF-8")
+    lines = f.readlines()
+    for line in lines:
+        if "Title:" in line:
+            if "Unicode" not in line:
+                title = (line.replace("Title:", "")).replace("\n", "")
+        if "Artist:" in line:
+            if "Unicode" not in line:
+                artist = (line.replace("Artist:", "")).replace("\n", "")
+
+    return song.Song(title, artist)
+
+
+def beatsaver(song, count):
+    url = "https://beatsaver.com/api/search/text/0/?q=" + urllib.parse.quote(song.title)
+    # very fucking dirty hack
+    hed = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) "
+                      "Chrome/39.0.2171.95 Safari/537.36"}
+
+    response = requests.get(url, headers=hed).json()
+    for x in range(count):
+        try:
+            got_title = response['docs'][x]["name"]
+            got_artist = response['docs'][x]['metadata']['songAuthorName']
+            dl = str(response['docs'][x]['stats']['downloads'])
+            up = str(response['docs'][x]['stats']['upVotes'])
+
+            if any(s in song.title.casefold().split(" ") for s in got_title.casefold().split(" ")):
+                if any(s in song.artist.casefold().split(" ") for s in got_artist.casefold().split(" ")):
+                    print('>[' + str(x) + '][LIKELY MATCH]---------')
+                else:
+                    print('>[' + str(x) + '][KINDA MATCH]----------')
+            elif song.title.casefold() in got_title.casefold():
+                print('>[' + str(x) + '][SAME NAME]------------')
+            else:
+                print('>[' + str(x) + ']-----------------------')
+
+            print('    Name: ' + got_title)
+
+            if got_artist:
+                print('    Artist: ' + got_artist)
+
+            print('    DL: ' + dl)
+            print('    UP: ' + up)
+        except IndexError:
+            print('>[NOT FOUND]--------------')
+            break
+    print('>[END]--------------------\n')
+
+
+if __name__ == "__main__":
+    song_list = []
+
+    # Path to Osu! songs
+    songs_dir = os.path.join("V:", "Games", "Osu!", "Songs")
+
+    # How many listings per song? (Weird way just for the convenience here)
+    count = 2
+
+    for track in os.listdir(songs_dir):
+        song_list.append(rann(os.path.join(songs_dir, track)))
+
+    print("Disclaimer: Many BeatSaber and Osu! mappers instead of song's artist name type in their own. It's not that "
+          "my code is THAT dumb. Those mappers are.")
+
+    for song in song_list:
+        print('Searching: ' + song.title + ' by ' + song.artist)
+        beatsaver(song, count)
+    print("DONE for " + str(len(songs_dir)) + " songs!")
